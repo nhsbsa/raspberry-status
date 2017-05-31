1060b4169f3d279cfbee9b78044a4a6ad9139188
Gary Brown <gary.brown3@nhs.net>
2017-05-31 09:55:28 +0000

    installation scripts and autostart added

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull origin master
+
+./details.sh
+./display-details.sh
---
+++ install-autostart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+currentdir=$(pwd)
+sudo echo $currentdir/bootstrap.sh >> /home/pi/.config/lxsession/LXDE-pi/autostart 
