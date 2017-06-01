3d604154c2b849966f74f288574b094d93be1a34
Gary Brown <gary.brown3@nhs.net>
2017-06-01 08:25:44 +0100

    stop screen blanking energy saving

--- install-autostart.sh
+++ install-autostart.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 currentdir=$(pwd)
-sudo echo "@lxterminal --command /home/pi/raspberry-status/bootstrap.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart 
+autostart=/home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "@lxterminal --command /home/pi/raspberry-status/bootstrap.sh" >> $autostart
 
+# Prevent screen from blanking out
+sudo echo "@xset s off         # don't activate screensaver" >> $autostart
+sudo echo "@xset -dpms         # disable DPMS (Energy Star) features." >> $autostart
+sudo echo "@xset s noblank     # don't blank the video device" >> $autostart
+
+sudo sed -i -e '/@xscreensaver -no-splash/c\\#@xscreensaver -no-splash' $autostart
