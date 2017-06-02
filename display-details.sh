cc84120f55dc70c7445dc12f272ea1b3a1f137c8
Gary Brown <gary.brown3@nhs.net>
2017-06-02 13:37:03 +0100

    display google analytics on load

--- display-details.sh
+++ display-details.sh
@@ -3,5 +3,5 @@ xset s off
 xset -dpms
 xset s noblank
 
-chromium-browser --start-fullscreen output.html
-
+#chromium-browser --start-fullscreen output.html
+chromium-browser https://analytics.google.com
